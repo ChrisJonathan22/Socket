d116da6f3568c12b3ccd05751a93ba9033e53553
Chris Jonathan <christophereko@hotmail.fr>
2018-12-22 10:11:38 +0000

    I have added some comments, deleted the about page which wasn't needed any longer, updated dependencies and I have removed node modules from the repo.

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
 const express = require('express');
 const app = express();
-const http = require('http').Server(app)
+const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = 3000;
 
+// Tell express to use the public folder for static files
+app.use(express.static('public'));
+
+// Create a route which responds to Get requests
 app.get('/', (req, res) => {
-    app.use(express.static('public'));
-    res.sendFile(__dirname + "/public/index.html");
+    // As a response send a file
+    res.sendFile(__dirname + '/public/index.html');
 });
 
+// When a connection has been established take in socket
 io.on('connection', (socket) => {
-    console.log('a user connected', socket.client.id);
-    
+    // On connection log a message with the socket id
+    console.log('a user is connected', socket.client.id);
+    // If the chat message event has been triggered receive msg
     socket.on('chat message', (msg) => {
+        // Trigger the chat message event and pass in the value received
         io.emit('chat message', msg);
-        console.log('message: ' + msg);
-      });
+        // Log the value
+        console.log(`message: ${msg}`);
+    });
+    // On disconnection, log a message with the socket id
     socket.on('disconnect', () => {
-        console.log('user disconnected'); 
+        console.log('a user is disconnected', socket.client.id);
     });
 });
 
-http.listen(port, console.log(`The server has started on port ${port}`)
-);
-
+http.listen(port, () => {
+    console.log(`listening on port: ${port}`); 
+});
