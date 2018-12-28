73518e38b2d4345160278e04cc5c2ca2020a0d5e
Chris Jonathan <christophereko@hotmail.fr>
2018-12-28 08:22:36 +0000

    I have added a feature which displays a message when a user is typing and when the user stops typing, the message is removed. I now need to find a way to only display the message to the user or users who aren't typing.

--- index.js
+++ index.js
@@ -4,34 +4,46 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = 3000;
 
-// Tell express to use the public folder for static files
-app.use(express.static('public'));
 
-// Create a route which responds to Get requests
-app.get('/', (req, res) => {
-    // As a response send a file
-    res.sendFile(__dirname + '/public/index.html');
+app.use(express.static('public'));  // Tell express to use the public folder for static files.
+
+
+app.get('/', (req, res) => {    // Create a route which responds to Get requests.
+    res.sendFile(__dirname + '/public/index.html'); // As a response send a file.
 });
 
-// When a connection has been established take in socket
-io.on('connection', (socket) => {
-    // On connection log a message with the socket id
-    console.log('a user is connected', socket.client.id);
-    // If the chat message event has been triggered receive msg
-    socket.on('chat message', (msg) => {
-        // Trigger the chat message event and pass in the value received
-        io.emit('chat message', msg);
-        // Log the value
-        console.log(`message: ${msg}`);
+io.on('connection', (socket) => {   // When a connection has been established take in socket.
+    console.log('a user is connected', socket.client.id);   // On connection log a message with the socket id.
+
+    socket.on('chat message', (msg) => {    // If the chat message event has been triggered receive msg.
+        
+        io.emit('chat message', msg);   // Trigger the chat message event and pass in the value received.
+        
+        console.log(`message: ${msg}`); // Log the value.
     });
-    // On disconnection, log a message with the socket.client.id
-    socket.on('disconnect', () => {
+
+    
+    socket.on('disconnect', () => { // On disconnection, log a message with the socket.client.id .
+
         console.log('a user is disconnected', socket.client.id);
-        // On disconnection send the socket.client.id to the client side
-        io.emit('terminated', socket.client.id);
+
+        io.emit('terminated', socket.client.id);    // On disconnection send the socket.client.id to the client side.
     });
-    // On connection send the socket.client.id to the client side
-    io.emit('connected', socket.client.id);
+
+    
+    io.emit('connected', socket.client.id);     // On connection send the socket.client.id to the client side.
+
+
+    socket.on('typing', (who) => {  // When a user types pass in who
+        io.emit('typing', who);     // Trigger the event and receive who which will be a string "a user is typing..." .
+        console.log(who);
+    });
+
+
+    socket.on('notTyping', (who) => {   // When a user stops typing pass in who.
+        io.emit('notTyping', who);      // Trigger the event and receive who which will be an empty string.
+        console.log(who, 'user no long typing...');
+    });
 });
 
 http.listen(port, () => {
--- public/chat.js
+++ public/chat.js
@@ -155,51 +155,60 @@ $(() => {
     'Toshie',
     'Junko'];
   
-    // When a user clicks inside the input field, show the element with an id of typing.
-    $('#m').click(()=> {
-        $('#typing').show();
-        // Once the element is shown add the text below.
-        $('#typing').text("a user is typing...");
+    
+    $('#m').click(()=> {    // When a user clicks inside the input field, show the element with an id of typing.  
+        
+        socket.emit('typing', "a user is typing...");   // Trigger the event and pass in the value.
     });
 
-    // When the user clicks outside the input field, hide the element with an id of typing.
-    $('#m').blur(() => {
-        $('#typing').hide();
+    
+    $('#m').blur(() => {    // When the user clicks outside the input field, hide the element with an id of typing.
+
+        socket.emit('notTyping', " ");   // Trigger the event.
     });
 
-    $('form').submit(() => {
-        // When a user submits his or her message, hide the element with an id of typing.
+    socket.on('typing', (who) => {  // Create an event for when a user types.
+        $('#typing').text(who);     // Receive the value sent and add or replace the content of #typing with the value received.
+    });
+
+    socket.on('notTyping', (who) => {   // Create an event for when a user stops typing.
+        $('#typing').text(who);         // Receive the value sent and add or replace the content of #typing with the value received.
+    });
+
+    $('form').submit(() => {    // When a user submits his or her message, hide the element with an id of typing.
         $('#typing').hide();
-        // Generate a random number between 1 & 152.
-       let randomNumber = Math.floor(Math.random() * 152) + 1;
-        // Log the random number.   
-       console.log(randomNumber);
+
+       let randomNumber = Math.floor(Math.random() * 152) + 1;  // Generate a random number between 1 & 152.
+    
+       console.log(randomNumber);   // Log the random number.   
        
        /*
-         When you submit the form emit an event called chat message
+         When you submit the form trigger an event called chat message
         which should match the event name setup in the backend
         and send the value of the input which will be received in
         the backend and then display it.
         Add the random number before the message.
         */ 
         socket.emit('chat message', `${names[randomNumber]}: ${$('#m').val()}`);
-        // After sending the data empty the input field.
-        $('#m').val('');
-        // Exit.
-        return false;
+
+        $('#m').val('');    // After sending the data empty the input field.
+        
+        return false;   // Exit.
     });
-    // If the chat message is triggered in the backend receive the value.
-    socket.on('chat message', (msg) => {
-        // Add the value to the page within an LI element.
-        $('#messages').append($('<li>').text(msg));
+
+    socket.on('chat message', (msg) => {    // Create an event called chat message which receives a value = msg.
+        
+        $('#messages').append($('<li>').text(msg)); // Add the value to the page within an LI element.
+    });
+
+    
+    socket.on('connected', (user) => {  // Create an event called connected which receives user.
+
+        $('#messages').append($('<li>').text(`Connected user: ${user}`));   // Receive user = socket.client.id and append it.
     });
     
-    // When connected receive the socket.client.id and append it.
-    socket.on('connected', (user) => {
-        $('#messages').append($('<li>').text(`Connected user: ${user}`));
-    });
-    // When disconnected receive the socket.client.id and append it.
-    socket.on('terminated', (user) => {
-        $('#messages').append($('<li>').text(`User disconnected: ${user}`));
+    socket.on('terminated', (user) => { // Create an event called terminated which receives user.
+
+        $('#messages').append($('<li>').text(`User disconnected: ${user}`));    // Receive user = socket.client.id and append it
     });
 });
