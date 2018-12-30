f9212fc8862008277169900797ba451dd320d96f
Chris Jonathan <christophereko@hotmail.fr>
2018-12-30 21:46:59 +0000

    I'm working on showing the correct names of connected users, remove disconnected users and messages sent should display the right author.

--- index.js
+++ index.js
@@ -4,6 +4,161 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = 3000;
 
+// This is an array of names which will be used as nicknames for every new message and connected users. The names will be picked by random.
+let names = ['Allison',
+    'Arthur',
+    'Ana',  
+    'Alex',
+    'Arlene',
+    'Alberto',
+    'Barry',
+    'Bertha',
+    'Bill',
+    'Bonnie',
+    'Bret',
+    'Beryl',
+    'Chantal',
+    'Cristobal',
+    'Claudette',
+    'Charley',
+    'Cindy',
+    'Chris',
+    'Dean',
+    'Dolly',
+    'Danny',
+    'Danielle',
+    'Dennis',
+    'Debby',
+    'Erin',
+    'Edouard',
+    'Erika',
+    'Earl',
+    'Emily',
+    'Ernesto',
+    'Felix',
+    'Fay',
+    'Fabian',
+    'Frances',
+    'Franklin',
+    'Florence',
+    'Gabielle',
+    'Gustav',
+    'Grace',
+    'Gaston',
+    'Gert',
+    'Gordon',
+    'Humberto',
+    'Hanna',
+    'Henri',
+    'Hermine',
+    'Harvey',
+    'Helene',
+    'Iris',
+    'Isidore',
+    'Isabel',
+    'Ivan',
+    'Irene',
+    'Isaac',
+    'Jerry',
+    'Josephine',
+    'Juan',
+    'Jeanne',
+    'Jose',
+    'Joyce',
+    'Karen',
+    'Kyle',
+    'Kate',
+    'Karl',
+    'Katrina',
+    'Kirk',
+    'Lorenzo',
+    'Lili',
+    'Larry',
+    'Lisa',
+    'Lee',
+    'Leslie',
+    'Michelle',
+    'Marco',
+    'Mindy',
+    'Maria',
+    'Michael',
+    'Noel',
+    'Nana',
+    'Nicholas',
+    'Nicole',
+    'Nate',
+    'Nadine',
+    'Olga',
+    'Omar',
+    'Odette',
+    'Otto',
+    'Ophelia',
+    'Oscar',
+    'Pablo',
+    'Paloma',
+    'Peter',
+    'Paula',
+    'Philippe',
+    'Patty',
+    'Rebekah',
+    'Rene',
+    'Rose',
+    'Richard',
+    'Rita',
+    'Rafael',
+    'Sebastien',
+    'Sally',
+    'Sam',
+    'Shary',
+    'Stan',
+    'Sandy',
+    'Tanya',
+    'Teddy',
+    'Teresa',
+    'Tomas',
+    'Tammy',
+    'Tony',
+    'Van',
+    'Vicky',
+    'Victor',
+    'Virginie',
+    'Vince',
+    'Valerie',
+    'Wendy',
+    'Wilfred',
+    'Wanda',
+    'Walter',
+    'Wilma',
+    'William',
+    'Kumiko',
+    'Aki',
+    'Miharu',
+    'Chiaki',
+    'Michiyo',
+    'Itoe',
+    'Nanaho',
+    'Reina',
+    'Emi',
+    'Yumi',
+    'Ayumi',
+    'Kaori',
+    'Sayuri',
+    'Rie',
+    'Miyuki',
+    'Hitomi',
+    'Naoko',
+    'Miwa',
+    'Etsuko',
+    'Akane',
+    'Kazuko',
+    'Miyako',
+    'Youko',
+    'Sachiko',
+    'Mieko',
+    'Toshie',
+    'Junko'];
+
+let currentUser;    // This is where the current user will be stored. 
 
 app.use(express.static('public'));  // Tell express to use the public folder for static files.
 
@@ -17,9 +172,11 @@ io.on('connection', (socket) => {   // When a connection has been established ta
 
     socket.on('chat message', (msg) => {    // If the chat message event has been triggered receive msg.
         
-        io.emit('chat message', msg);   // Trigger the chat message event and pass in the value received.
+        io.emit('chat message', `${names[currentUser]}${msg}`);   // Trigger the chat message event and pass in the value received.
         
         console.log(`message: ${msg}`); // Log the value.
+        console.log(names[currentUser]);
+        
     });
 
     
@@ -27,11 +184,12 @@ io.on('connection', (socket) => {   // When a connection has been established ta
 
         console.log('a user is disconnected', socket.client.id);
 
-        io.emit('terminated', socket.client.id);    // On disconnection send the socket.client.id to the client side.
+        io.emit('terminated', names[currentUser]);    // On disconnection send the socket.client.id to the client side.
     });
 
-    
-    io.emit('connected', socket.client.id);     // On connection send the socket.client.id to the client side.
+    currentUser = Math.floor(Math.random() * 152) + 1; // Generate a random number.
+
+    io.emit('connected', names[currentUser]);     // On connection send the socket.client.id to the client side.
 
 
     socket.on('typing', (who) => {  // When a user types pass in who
@@ -42,7 +200,7 @@ io.on('connection', (socket) => {   // When a connection has been established ta
 
     socket.on('notTyping', (who) => {   // When a user stops typing pass in who.
         io.emit('notTyping', who);      // Trigger the event and receive who which will be an empty string.
-        console.log(who, 'user no long typing...');
+        console.log(who, 'user no longer typing...');
     });
 });
 
